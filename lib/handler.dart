547a756ab86dbccced96d9dc84e458faeecf385d
Tabata Llach <tllach@uninorte.edu.co>
2024-10-07 16:58:58 -0500

    handler, reusable component made

---
+++ lib/handler.dart
@@ -0,0 +1,9 @@
+import './ui/components/logo.dart';
+
+/**
+ * This handler will be use to get reusable components and functions.
+ */
+
+Logo getLogo(double height, double width) {
+  return Logo(height, width);
+}
