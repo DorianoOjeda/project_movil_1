d58adb282dc560065fd2200f96fe5d778a13b531
Tabata Llach <tllach@uninorte.edu.co>
2024-10-10 22:46:59 -0500

    calendar page added

--- lib/handler.dart
+++ lib/handler.dart
@@ -1,7 +1,8 @@
 import './ui/components/logo.dart';
 import './ui/components/navigationbtn.dart';
-import './ui/components/calendar.dart';
+import 'ui/components/calendar_widget.dart';
 import 'ui/pages/home.dart';
+import 'ui/pages/calendar.dart';
 
 /// This handler will be use to get reusable components and functions.
 
@@ -13,11 +14,15 @@ NavigationButton getNavigationButton(String text, String route) {
   return NavigationButton(text, route);
 }
 
-Calendar getCalendar() {
-  return const Calendar();
+CalendarWidget getCalendarWidget() {
+  return const CalendarWidget();
 }
 
 /// getPages
 HomePage getHomePage() {
   return const HomePage();
+}
+
+Calendar getCalendar() {
+  return const Calendar();
 }
--- lib/ui/components/calendar_widget.dart
+++ lib/ui/components/calendar_widget.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({super.key});
+class CalendarWidget extends StatefulWidget {
+  const CalendarWidget({super.key});
   @override
-  State<Calendar> createState() => _CalendarState();
+  State<CalendarWidget> createState() => _CalendarWidgetState();
 }
 
-class _CalendarState extends State<Calendar> {
+class _CalendarWidgetState extends State<CalendarWidget> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 210, 185, 255),
+        border: Border.all(
+          color: Colors.black, // Color del borde
+          width: 1.5, // Grosor del borde
+        ),
+      ),
+      padding: const EdgeInsets.only(
+          top: 10.0, bottom: 20.0, left: 10.0, right: 10.0),
       child: TableCalendar(
         firstDay: DateTime.utc(2021, 1, 1),
         lastDay: DateTime.utc(2030, 12, 31),
         focusedDay: _focusedDay,
         calendarFormat: _calendarFormat,
+        //function
         selectedDayPredicate: (day) {
           return isSameDay(_selectedDay, day);
         },
