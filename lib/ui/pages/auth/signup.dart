51e9d3f4f78b942f1eede18cd9d4f8a91d1beaa8
Tabata Llach <tllach@uninorte.edu.co>
2024-10-15 00:03:43 -0500

    fix bottom overflow in auth pages

--- lib/ui/pages/auth/signup.dart
+++ lib/ui/pages/auth/signup.dart
@@ -15,51 +15,61 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Color(0xFF4A148C), Color(0xFF12005E)], // Fondo degradado
-            ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF4A148C), Color(0xFF12005E)],
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Form(
-              key: _formkey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 100.0,
-                      bottom: 20.0,
+        ),
+        child: SafeArea(
+            child: Center(
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            padding: const EdgeInsets.all(10.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight:
+                    MediaQuery.of(context).size.height - 50, // Ajusta la altura
+              ),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 40.0,
+                        bottom: 20.0,
+                      ),
+                      child: Center(
+                        child: getLogo(150, 150),
+                      ),
                     ),
-                    child: Center(
-                      child: getLogo(150, 150),
-                    ),
-                  ),
-                  const Center(
+                    const Center(
                       child: Text(
-                    "Mi Mejor Ser",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
+                        "Mi Mejor Ser",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  )),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      validator: MultiValidator([
-                        RequiredValidator(
-                            errorText: 'Please enter your first name'),
-                        MinLengthValidator(3,
-                            errorText:
-                                'First name must be at least 3 characters long'),
-                      ]).call,
-                      decoration: const InputDecoration(
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        validator: MultiValidator([
+                          RequiredValidator(
+                              errorText: 'Please enter your first name'),
+                          MinLengthValidator(3,
+                              errorText:
+                                  'First name must be at least 3 characters long'),
+                        ]).call,
+                        decoration: const InputDecoration(
                           labelStyle: TextStyle(color: Colors.white),
                           hintText: 'Enter first name',
                           labelText: 'First name',
@@ -69,123 +79,129 @@ class _SignupPageState extends State<SignupPage> {
                           ),
                           errorStyle: TextStyle(fontSize: 18.0),
                           border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(9.0)))),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      validator: MultiValidator([
-                        RequiredValidator(
-                            errorText: 'Please enter your last name'),
-                        MinLengthValidator(3,
-                            errorText:
-                                'Last name must be at least 3 characters long'),
-                      ]).call,
-                      decoration: const InputDecoration(
-                          labelStyle: TextStyle(color: Colors.white),
-                          hintText: 'Enter last name',
-                          labelText: 'Last name',
-                          prefixIcon: Icon(
-                            Icons.person,
-                            color: Colors.grey,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(9.0)),
                           ),
-                          errorStyle: TextStyle(fontSize: 18.0),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(9.0)))),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      validator: MultiValidator([
-                        RequiredValidator(
-                            errorText: 'Please enter your email address'),
-                        EmailValidator(
-                            errorText: 'Please enter a valid email address'),
-                      ]).call,
-                      decoration: const InputDecoration(
-                          labelStyle: TextStyle(color: Colors.white),
-                          hintText: 'Email',
-                          labelText: 'Email',
-                          prefixIcon: Icon(
-                            Icons.email,
-                            //Color.fromARGB(255, 112, 2, 255),
-                            color: Colors.grey,
-                          ),
-                          errorStyle: TextStyle(fontSize: 18.0),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(9.0)))),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      obscureText: true, // Para ocultar la contraseña
-                      keyboardType: TextInputType.text,
-                      validator: MultiValidator([
-                        RequiredValidator(errorText: 'Please enter a password'),
-                        MinLengthValidator(8,
-                            errorText:
-                                'Password must be at least 8 characters long'),
-                        PatternValidator(r'(?=.*?[#_?!@$%^&*-])',
-                            errorText:
-                                'Passwords must have at least one special character'),
-                      ]).call,
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Colors.white),
-                        hintText: 'Password',
-                        labelText: 'Password',
-                        prefixIcon: Icon(
-                          Icons.lock,
-                          color: Colors.grey,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(9)),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 112, 2, 255),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        child: const Text(
-                          'Register',
-                          style: TextStyle(color: Colors.white, fontSize: 22),
-                        ),
-                        onPressed: () {
-                          if (_formkey.currentState!.validate()) {
-                            // Form is valid, proceed with your logic
-                            Navigator.pushNamed(
-                              context,
-                              '/initialPage',
-                            );
-                          }
-                        },
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        validator: MultiValidator([
+                          RequiredValidator(
+                              errorText: 'Please enter your last name'),
+                          MinLengthValidator(3,
+                              errorText:
+                                  'Last name must be at least 3 characters long'),
+                        ]).call,
+                        decoration: const InputDecoration(
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintText: 'Enter last name',
+                            labelText: 'Last name',
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Colors.grey,
+                            ),
+                            errorStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(9.0)))),
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        validator: MultiValidator([
+                          RequiredValidator(
+                              errorText: 'Please enter your email address'),
+                          EmailValidator(
+                              errorText: 'Please enter a valid email address'),
+                        ]).call,
+                        decoration: const InputDecoration(
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintText: 'Email',
+                            labelText: 'Email',
+                            prefixIcon: Icon(
+                              Icons.email,
+                              //Color.fromARGB(255, 112, 2, 255),
+                              color: Colors.grey,
+                            ),
+                            errorStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(9.0)))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        obscureText: true, // Para ocultar la contraseña
+                        keyboardType: TextInputType.text,
+                        validator: MultiValidator([
+                          RequiredValidator(
+                              errorText: 'Please enter a password'),
+                          MinLengthValidator(8,
+                              errorText:
+                                  'Password must be at least 8 characters long'),
+                          PatternValidator(r'(?=.*?[#_?!@$%^&*-])',
+                              errorText:
+                                  'Passwords must have at least one special character'),
+                        ]).call,
+                        decoration: const InputDecoration(
+                          labelStyle: TextStyle(color: Colors.white),
+                          hintText: 'Password',
+                          labelText: 'Password',
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(9)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 112, 2, 255),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          child: const Text(
+                            'Register',
+                            style: TextStyle(color: Colors.white, fontSize: 22),
+                          ),
+                          onPressed: () {
+                            if (_formkey.currentState!.validate()) {
+                              // Form is valid, proceed with your logic
+                              Navigator.pushNamed(
+                                context,
+                                '/initialPage',
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          )),
+          ),
+        )),
+      ),
     );
   }
 }
